4c39b071424b98e30547a0d9d840d60ac0cc7719
danielkamweru <daniel.kamweru@student.moringaschool.com>
2026-01-16 23:36:33 +0300

    Initial commit: Banking App Frontend

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { NotificationProvider } from './context/NotificationContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <NotificationProvider>
-      <App />
-    </NotificationProvider>
+    <App />
   </React.StrictMode>,
 )
